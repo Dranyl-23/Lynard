f90c49658964e3a0462735a7db467cad8b34fc49
Dranyl <Alfielynard23@gmail.com>
2026-07-10 00:16:39 +0800

    fix: add fallback favicon and cache buster

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,7 +25,8 @@
     <meta property="twitter:description" content="{{ $description }}">
     <meta property="twitter:image" content="{{ asset($image) }}">
 
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=2">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v=2">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
